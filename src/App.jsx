6dace7414677abafcb5a17a2cb1431b9dd8b61d7
Javier Vivas <javier.vivas@icloud.com>
2023-07-19 19:40:39 +0200

    delete style Button

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import EventDetailPage from "./pages/privates.routes/event/EventDetailPage";
 import TracksPage from "./pages/privates.routes/playlist/TracksPage";
 import PlaylistOfListPage from "./pages/privates.routes/playlist/PlaylistOfListPage";
 import UserPageEdit from "./pages/privates.routes/user/UserPageEdit";
-import OnBoardingPage from "./pages/onBoardingPage";
+import OnBoardingPage from "./pages/OnBoardingPage";
 import TypeOfUserPage from "./pages/typeOfUserPage";
 
 function App() {
@@ -102,7 +102,7 @@ function App() {
             </PrivateRoute>
           }
         />
-        
+
         <Route path="/user-type" element={<TypeOfUserPage />} />
         <Route path="/" element={<OnBoardingPage />} />
         <Route path="/login" element={<LoginPage />} />
