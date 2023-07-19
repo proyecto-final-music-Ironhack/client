d457f1d0ed8bfd9ec835c4e13b8b2bef7c99eb24
Raquel de Frutos <raqueldfrutos@gmail.com>
2023-07-19 08:44:14 +0200

    Now Playing modify, tracks ranking fixed

--- src/components/Playlist/TrackCard.jsx
+++ src/components/Playlist/TrackCard.jsx
@@ -1,60 +1,61 @@
-import { Button, Image, Spinner } from '@chakra-ui/react'
-import emptyHeart from '../../images/empy-heart.png'
-import heart from '../../images/heart.png'
-import playlistService from '../../services/playlist.service'
-import { useState, useEffect } from 'react'
+import { Button, Image, Spinner } from "@chakra-ui/react";
+import emptyHeart from "../../images/empy-heart.png";
+import heart from "../../images/heart.png";
+import playlistService from "../../services/playlist.service";
+import { useState, useEffect } from "react";
 
 function TrackCard({ trackName, likes, image, artists, _id, userId }) {
-  const [like, setLike] = useState(likes?.includes(userId) ?? false)
-  const [showLikes, setShowLikes] = useState(likes?.length ?? 0)
-  const [artistList, setArtistList] = useState('')
+  const [like, setLike] = useState(likes?.includes(userId) ?? false);
+  const [showLikes, setShowLikes] = useState(likes?.length ?? 0);
+  const [artistList, setArtistList] = useState("");
 
   useEffect(() => {
     if (artists) {
-      setArtistList(artists.join(', '))
+      setArtistList(artists.join(", "));
     }
-  }, [artists])
+  }, [artists]);
 
   const handleLike = async () => {
     try {
-      await playlistService.getTrackLike(_id)
-      setLike(true)
-      setShowLikes(num => num + 1)
+      await playlistService.getTrackLike(_id);
+      setLike(true);
+      setShowLikes((num) => num + 1);
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+  };
 
   const handleDislike = async () => {
     try {
-      await playlistService.getTrackDislike(_id)
-      setLike(false)
-      setShowLikes(num => (num > 0 ? num - 1 : 0))
+      await playlistService.getTrackDislike(_id);
+      setLike(false);
+      setShowLikes((num) => (num > 0 ? num - 1 : 0));
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+  };
 
   return (
     <>
-      <img src={image} alt='Track image' />
+      <img src={image} alt="Track image" />
       <p>{trackName}</p>
       <p>{artistList || <Spinner />}</p>
-      <p style={{ display: 'inline-block' }}>{showLikes >= 0 ? showLikes : <Spinner/>}</p>
-
+      <>
+        <p style={{ display: "inline-block" }}>{showLikes >= 0 ? showLikes : <Spinner />}</p>
+      </>
       {like ? (
-        <Button className='like-button' type='button' onClick={handleDislike}>
-          <Image w='18px' src={heart} />
+        <Button className="like-button" type="button" onClick={handleDislike}>
+          <Image w="18px" src={heart} />
         </Button>
       ) : (
-        <Button className='dislike-button' type='button' onClick={handleLike}>
-          <Image w='18px' src={emptyHeart} />
+        <Button className="dislike-button" type="button" onClick={handleLike}>
+          <Image w="18px" src={emptyHeart} />
         </Button>
       )}
 
       <hr />
     </>
-  )
+  );
 }
 
-export default TrackCard
+export default TrackCard;
