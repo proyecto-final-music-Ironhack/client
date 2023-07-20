083945b5401b3df0ab001fbdd029a24e3f4b8f56
Raquel de Frutos <raqueldfrutos@gmail.com>
2023-07-20 17:06:38 +0200

    Styles: Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import { ReactNode, useContext } from "react";
 import { Link } from "react-router-dom";
-import { Avatar, Box, Spacer, Flex, Image, Menu, MenuButton, MenuList, Button, MenuItem } from "@chakra-ui/react";
+import { Avatar, Box, Spacer, Flex, Image, Menu, MenuButton, MenuList, Button, MenuItem, Container } from "@chakra-ui/react";
 import logo from "../images/logo-beat.png";
 import { AuthContext } from "../context/auth.context";
 
@@ -12,27 +12,29 @@ export default function Nav() {
   return (
     <>
       {user && (
-        <Flex className="navbar, bg-black" alignItems={"center"} justifyContent={"space-between"}>
-          <Box p="20">
+        <Container p="30px">
+        <Flex className="navbar" alignItems={"center"} justifyContent={"space-between"}>
+          <Box >
             <Link to="/events">
               <Image w="74px" name="Beat the Beat Logo" src={logo} />
             </Link>
           </Box>
           <Spacer />
           <Menu style={{ zIndex: 10 }}>
-            <MenuButton className="profile-image" as={Button}>
+            <MenuButton className="profile-image">
               <Image boxSize="50px" borderRadius={500} name="profileImage" src={user.image} />
             </MenuButton>
             <MenuList>
               <Link to="/profile">
-                <MenuItem bg={"#4E4E4E"}>Profile</MenuItem>
+                <MenuItem >Profile</MenuItem>
               </Link>
-              <MenuItem bg={"#4E4E4E"} onClick={handleLogout}>
+              <MenuItem onClick={handleLogout}>
                 Log out
               </MenuItem>
             </MenuList>
           </Menu>
         </Flex>
+        </Container>
       )}
     </>
   );
